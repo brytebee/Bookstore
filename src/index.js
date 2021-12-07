d3d9da1e4b49b0742c24ccc3fbf66f4a8d793ae0
brytebee <brytebee@gmail.com>
2021-12-07 14:40:57 +0100

    Update CRLF to LF space style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Welcome = () => <div>
-  <h1>Welcome</h1>
-  <p>Hello world!</p>
-</div>
+const Welcome = () => (
+  <div>
+    <h1>Welcome</h1>
+    <p>Hello world!</p>
+  </div>
+);
 ReactDOM.render(
   <React.StrictMode>
     <Welcome />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
