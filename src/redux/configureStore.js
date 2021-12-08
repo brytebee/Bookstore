c7278123f26d797c6dd83b5cb90893afdf3d4087
brytebee <brytebee@gmail.com>
2021-12-08 16:28:18 +0100

    :white_check_mark: Update ConfigireStore
    Add given template code to file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,3 +1,12 @@
-const ConfigureStore = () => {};
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import booksReducer from './books/books';
 
-export default ConfigureStore;
+const reducer = combineReducers({
+  booksReducer,
+  // additional reducers could be added here
+});
+
+const store = createStore(reducer, applyMiddleware(logger));
+
+export default store;
