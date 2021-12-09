66be3ed1dd18481f5113df930250e8a74174e87d
brytebee <brytebee@gmail.com>
2021-12-09 14:00:37 +0100

    :white_check_mark: Linter
    Unstable behaviour

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
 import Books from './books/Books';
 import Categories from './categories/Categories';
