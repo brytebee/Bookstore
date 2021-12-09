daeeb594e58f388a365884097f932bc7ec32fc88
brytebee <brytebee@gmail.com>
2021-12-09 07:38:49 +0100

    :white_check_mark: Update books.js
    Complete the reducer function

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -7,19 +7,23 @@ export const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
 });
-export const removeBook = () => ({
+
+export const removeBook = (id) => ({
   type: REMOVE_BOOK,
+  id,
 });
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
+      return [...state, action.payload];
     /*
         return a new state object in which the books array will contain a new book object, passed by action.payload.
         Remember -  you MUSN'T mutate the state. You have to return a new state object - i.e.:
         return [ ...state, action.payload];
         */
     case REMOVE_BOOK:
+      return state.filter((book) => book.id !== id);
     /*
         use ES6 filter() method to create a new array, which will not contain the book you want to remove from the store (filter by the id key - i.e.: 
         return state.filter(book => book.id !== id);
