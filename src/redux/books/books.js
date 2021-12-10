0cdd415e3d40eaf1a9f28a46fe97ea81576db2c9
brytebee <brytebee@gmail.com>
2021-12-10 07:36:28 +0100

    :white_check_mark: Update files
    Update configureStore.js, install thunk, update books.js to use API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,17 +1,58 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_BOOK = 'bookStore/books/GET_BOOK';
+const baseUrl =
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0MxoaYAlLWZ6BQnrAqvb/books/';
 
 const initialState = [];
 
-export const addBook = (payload) => ({
+const addBook = (payload) => ({
   type: ADD_BOOK,
-  payload,
+  payload: {
+    ...payload,
+    progress: randomCompletion(),
+    chapter: chapter(),
+    author: 'Bright',
+  },
 });
 
-export const removeBook = (id) => ({
+const removeBook = (id) => ({
   type: REMOVE_BOOK,
   id,
 });
+
+const getBook = (payload) => ({
+  type: GET_BOOK,
+  payload,
+});
+
+export const addNewBook = (newBook) => async (dispatch) => {
+  await fetch(baseUrl, {
+    method: 'POST',
+    body: JSON.stringify(newBook),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  dispatch(addBook(newBook));
+};
+
+export const getBooks = () => async (dispatch) => {
+  const books = await fetch(baseUrl);
+  const bookData = await books.json();
+  dispatch(getBook(bookData));
+};
+
+export const removeBookFromStore = (id) => async (dispatch) => {
+  await fetch(`${baseUrl}${itemId}`, {
+    method: 'DELETE',
+    body: JSON.stringify({ item_id: id }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  dispatch(removeBook(id));
+};
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -19,6 +60,17 @@ const bookReducer = (state = initialState, action) => {
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
+    case GET_BOOK:
+      return Object.entries(action.payload).map(([key, value]) => {
+        const [book] = value;
+        return {
+          item_id: key,
+          ...book,
+          progress: randomCompletion(),
+          chapter: chapter(),
+          author: 'Bright',
+        };
+      });
     default:
       return state;
   }
