dea3a6c66158bd8badb62a3e16f455fa883115a1
brytebee <brytebee@gmail.com>
2021-12-11 14:59:39 +0100

    :white_check_mark: Add font family
    Add font family link

--- src/components/App.js
+++ src/components/App.js
@@ -6,23 +6,27 @@ import Books from './books/books';
 import Categories from './categories/categories';
 
 const App = () => (
-  <Router>
-    <nav>
-      <h1 id="logo">Bookstore CMS</h1>
-      <ul>
-        <li>
-          <Link to="/">Books</Link>
-        </li>
-        <li>
-          <Link to="/categories">Categories</Link>
-        </li>
-      </ul>
-    </nav>
+  <div className="panel-bg">
+    <Router>
+      <nav>
+        <h1 className="bookstore-CMS" id="logo">
+          Bookstore CMS
+        </h1>
+        <ul>
+          <li>
+            <Link to="/">Books</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
+        </ul>
+      </nav>
 
-    <Routes>
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/" element={<Books />} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/" element={<Books />} />
+      </Routes>
+    </Router>
+  </div>
 );
 export default App;
